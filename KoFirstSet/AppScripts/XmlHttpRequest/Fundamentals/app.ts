b8457e25fa32987531b27941efa355c3a0f1fff5
mko <gogs@fake.local>
2019-07-03 15:48:25 +0200

    Tasks prepared

--- KoFirstSet/AppScripts/XmlHttpRequest/Fundamentals/app.ts
+++ KoFirstSet/AppScripts/XmlHttpRequest/Fundamentals/app.ts
@@ -15,39 +15,9 @@
         let data: any = {};
         data.data = inputText;
 
-        try {
-            let r = await this.postData("http://localhost:50610/XmlHttpRequest/Fundamentals_Post", data);
-            console.log("Data retrieved.");
-            console.log(r);
-        }
-        catch (e) {
-            console.log("Data retrieval error.");
-        }
     };
 
-    postData(uri: string, data: any): Promise<any> {
-        let promise = new Promise<any>((resolve, reject) => {
-            let r: XMLHttpRequest = new XMLHttpRequest();
 
-            r.onreadystatechange = function () {
-                if (r.readyState === 4) {
-                    if (r.status === 200) {
-                        let response = JSON.parse(r.responseText);
-                        resolve(response);
-                    }
-                    else {
-                        reject();
-                    }
-                }
-            }
-
-            r.open("POST", uri);
-            r.setRequestHeader("content-type", "application/json");
-            r.send(JSON.stringify(data));
-        });
-
-        return promise;
-    }
 }
 
 let myApp: MyApp = new MyApp();
---
+++ KoFirstSet/AppScripts/XmlHttpRequest/Fundamentals/tasks.md
@@ -0,0 +1,26 @@
+﻿Using XMLHttpRequest for simple JSON post
+---
+
+Go to:
+http://localhost:50610/XmlHttpRequest/Fundamentals
+
+You have XmlHttpRequestController.Fundamentals_Post that accepts objects like:
+```
+{ data: "Sample text" }
+```
+For data==="Hello" it returns an error.
+
+In AppScripts / XmlHttpRequest / app.ts
+
+1. Create a method:
+```
+postData(uri: string, data: any): Promise<any>
+```
+Post any JS object (serializable to JSON) and returns deserialized response as a JS object.
+The method should reject the promise on all errors.
+- why can't you retrieve JS object directly.
+
+2. In sendClick call this method, on success log the response object to the console, for an error just log a text "Error".
+
+Test everthing for any text (success) and "Hello" (error).
+
