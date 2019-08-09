ec13f2c9295535e4b9efdc83f8e7fc1c4adff1d0
mko <gogs@fake.local>
2019-08-09 09:22:05 +0200

    corrected starter

--- KoFirstSet/AppScripts/Moment/Duration/app.ts
+++ KoFirstSet/AppScripts/Moment/Duration/app.ts
@@ -1,11 +1,12 @@
-﻿import "moment";
-
+﻿
 export class MyApp {
     constructor() {
         console.log("Durations loaded...");
+
     }
 }
- 
+
+
 
 let myApp: MyApp = new MyApp();
 (<any>window).myApp = myApp;
